d806773fc1983baba7c14bf31b73afa8979b7c8c
erl67 <erl67@pitt.edu>
2017-11-20 14:50:08 -0500

    add charset switch

--- IS1074password/src/is1074/password/PasswordCracker.java
+++ IS1074password/src/is1074/password/PasswordCracker.java
@@ -3,6 +3,7 @@ package is1074.password;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import com.google.common.base.Stopwatch; //using Guava timer
 
@@ -47,12 +48,31 @@ public class PasswordCracker {
 			System.out.println("No match with dictionary words, trying special characters");
 
 			// build array of possible special combinations, could use
-			// string.tochararray() but faster to do indvidually
-			char[] chars = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', '0', '{', '}', '-' };
-			//char[] chars = { '1', '!'}; //chair1! = 1.089 s
+			// string.tochararray() but faster to use stream (about 5x)
+			// char[] chars = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', '0', '{', '}', '-' };
 
+			ArrayList<String> charsList = new ArrayList<String>();
 			ArrayList<String> charCombos = new ArrayList<String>();
-
+			
+			int charset = 99;
+			
+	        switch (charset) {
+	            case 0:  charsList.add("0)");
+	            case 9:  charsList.add("9(");
+	            case 8:  charsList.add("8*");
+	            case 7:  charsList.add("7&");
+	            case 6:  charsList.add("6^");
+	            case 5:  charsList.add("5%");
+	            case 4:  charsList.add("4$");					//chair1! = 1.02m, chair1=1.037m
+	            case 3:  charsList.add("3#");					//chair1! = 21.54s
+	            case 2:  charsList.add("2@");					//chair1! = 5.852s
+	            case 1:  charsList.add("1!"); break;			//chair1! = 1.094s
+	            case 10:  charsList.add("12344567890"); break; 	//chair1 = 3.237m
+	            default:  charsList.add("0123456789@#$%&");
+	        }
+	        
+	        char[] chars = charsList.stream().collect(Collectors.joining()).toCharArray();	//convert string list to char array
+ 			
 			for (char special0 : chars) {
 				charCombos.add(String.valueOf(special0));
 				for (char special1 : chars) {
@@ -83,6 +103,7 @@ public class PasswordCracker {
 				}
 
 			}
+
 		}
 		return cracked;
 	}
