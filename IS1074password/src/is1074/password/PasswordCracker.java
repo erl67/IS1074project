1b848ed05f52810bf83a425703c62a0812d15d98
erl67 <erl67@pitt.edu>
2017-11-23 09:10:37 -0500

    clean up

--- IS1074password/src/is1074/password/PasswordCracker.java
+++ IS1074password/src/is1074/password/PasswordCracker.java
@@ -12,7 +12,7 @@ public class PasswordCracker {
 	public static void main(String[] args) {
 
 		HashMap<String, String> userMap = FileManager.readUsers();
-		ArrayList<String> wordList = FileManager.getDictionary(2);	//Optimized to only use words of minimum length, based on password rules
+		ArrayList<String> wordList = FileManager.getDictionary(2); // Optimized to only use words of minimum length, based on password rules
 
 		System.out.print("Enter username of password to crack: ");
 		Scanner scanner = new Scanner(System.in);
@@ -47,14 +47,14 @@ public class PasswordCracker {
 		if (!cracked) {
 			System.out.println("No match with dictionary words, trying special characters");
 
-			// build array of possible special combinations, could use string.tochararray() but was faster to use stream (about 5x)
+			// build array of possible special combinations, could also use string.tochararray() but was faster to use stream
 			// char[] chars = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&' };
 
 			ArrayList<String> charsList = new ArrayList<String>();
 			ArrayList<String> charCombos = new ArrayList<String>();
-			
-			int charset = 99;	//added a switch to allow for different charsets and compare runtimes
-			
+
+			int charset = 99;									// added a switch to allow for different charsets and compare runtimes
+
 	        switch (charset) {
 	            case 0:  charsList.add("0)");					//177240 variants per word, total variants for x specialchars = (x^2+x+1)^(x^2+x+1)-1
 	            case 9:  charsList.add("9(");					//117648 words
@@ -68,16 +68,11 @@ public class PasswordCracker {
 	            case 1:  charsList.add("1!"); break;			//48 words, chair1! = 1.094s
 	            case 10:  charsList.add("12344567890"); break; 	//17688 words, chair1 = 3.237m
 	            case 11:  charsList.add("1"); break; 			//2 combos c=x^2+x, 8 words w=(c+1)^(c+1)-1
-	            case 12:  charsList.add("12"); break; 			//6 combos, 48 words
-	            case 13:  charsList.add("123"); break; 			//12 combos, 168 words
-	            case 14:  charsList.add("1234"); break; 		//20 combos, 440 words
-	            case 15:  charsList.add("12345"); break; 		//30 combos, 960 words
-	            case 16:  charsList.add("123456"); break; 		//42 combos, 1848 words
 	            default:  charsList.add("0123456789@#$%&");		//240 combos, 58080 words, chair1 = 14.34m
 	        }
 	        
-	        char[] chars = charsList.stream().collect(Collectors.joining()).toCharArray();	//convert string list to char array for iterating
- 			
+			char[] chars = charsList.stream().collect(Collectors.joining()).toCharArray(); // convert string list to char array for iterating
+
 			for (char special0 : chars) {
 				charCombos.add(String.valueOf(special0));
 				for (char special1 : chars) {
@@ -89,17 +84,15 @@ public class PasswordCracker {
 				System.out.println(plaintext);
 				ArrayList<String> type2words = new ArrayList<String>();
 
-				for (String outer : charCombos) {		//create all type 2 passwords for a given word.
+				for (String outer : charCombos) { // create all type 2 passwords for a given word.
 					type2words.add(plaintext + outer);
 					type2words.add(outer + plaintext);
-					//System.out.println(plaintext + outer + "\t" + outer + plaintext);
 					for (String inner : charCombos) {
 						type2words.add(outer + plaintext + inner);
 					}
 				}
 
-				for (String type2plaintext : type2words) {		//test hash of new type2 words against password hash
-					//System.out.println(type2plaintext);
+				for (String type2plaintext : type2words) { // test hash of new type2 words against password hash
 					if (JavaMD5Hash.md5(type2plaintext).equals(testPassword)) {
 						System.out.println("Password match: " + type2plaintext);
 						cracked = true;
@@ -107,13 +100,6 @@ public class PasswordCracker {
 					}
 				}
 
-//				try {
-//					System.out.println(wordList.size());
-//					System.out.println(charCombos.size() + " " + charCombos.toString());
-//					System.out.println(type2words.size() + " " + type2words.toString());
-//					Thread.sleep(10000);
-//				} catch (InterruptedException e) {e.printStackTrace();}
-				
 			}
 
 		}
